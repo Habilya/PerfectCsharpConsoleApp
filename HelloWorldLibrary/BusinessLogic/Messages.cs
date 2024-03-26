4a4c89d25a9700964f8ce0e0712d072fc653c8ef
Andrei Ciuvilin <aciuvilin@morneaushepell.com>
2024-03-26 16:40:20 -0400

    First commit

---
+++ HelloWorldLibrary/BusinessLogic/Messages.cs
@@ -0,0 +1,55 @@
+﻿using HelloWorldLibrary.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace HelloWorldLibrary.BusinessLogic;
+
+public class Messages : IMessages
+{
+	private const string TRANSLATIONS_JSON_FILENAME = "CustomText.json";
+	private readonly ILogger<Messages> _log;
+
+	public Messages(ILogger<Messages> log)
+	{
+		_log = log;
+	}
+
+	public string Greeting(string language)
+	{
+		return LookUpCustomText("Greeting", language);
+	}
+
+	private string LookUpCustomText(string key, string language)
+	{
+		JsonSerializerOptions options = new()
+		{
+			PropertyNameCaseInsensitive = true,
+		};
+
+
+		try
+		{
+			List<CustomText>? messageSets = JsonSerializer
+			.Deserialize<List<CustomText>>
+			(
+				File.ReadAllText(TRANSLATIONS_JSON_FILENAME), options
+			);
+
+			CustomText? messages = messageSets?
+				.Where(q => q.Language.Equals(language))
+				.First();
+
+			if (messages is null)
+			{
+				throw new NullReferenceException($"The specified language was not found in the {TRANSLATIONS_JSON_FILENAME}");
+			}
+
+			return messages.Translations[key];
+		}
+		catch (Exception ex)
+		{
+			_log.LogError("Unhandled exception", ex);
+			throw;
+		}
+	}
+}
---
+++ HelloWorldTests/BusinessLogic/MessagesTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using HelloWorldLibrary.BusinessLogic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace HelloWorldTests.BusinessLogic;
+
+public class MessagesTests
+{
+	[Fact]
+	public void Greeting_InEnglish()
+	{
+		ILogger<Messages> logger = new NullLogger<Messages>();
+		Messages messages = new(logger);
+
+
+		string expected = "Hello World";
+		string actual = messages.Greeting("en");
+
+		actual.Should().Be(expected);
+	}
+
+	[Fact]
+	public void Greeting_InFrench()
+	{
+		ILogger<Messages> logger = new NullLogger<Messages>();
+		Messages messages = new(logger);
+
+
+		string expected = "Bonjour monde";
+		string actual = messages.Greeting("fr");
+
+		actual.Should().Be(expected);
+	}
+
+	[Fact]
+	public void Greeting_Invalid()
+	{
+		ILogger<Messages> logger = new NullLogger<Messages>();
+		Messages messages = new(logger);
+
+
+		Action failingRun = () => { messages.Greeting("invalid language"); };
+
+		failingRun.Should().Throw<InvalidOperationException>();
+	}
+}
