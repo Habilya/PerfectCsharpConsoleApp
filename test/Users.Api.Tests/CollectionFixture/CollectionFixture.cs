d284ec559158fc84b354d61a95de8ebc661a7626
Andrei Ciuvilin <aciuvilin@morneaushepell.com>
2024-05-08 14:30:04 -0400

    Add Collection fixture tests shared between 2 test classes

---
+++ test/Users.Api.Tests/CollectionFixture/CollectionFixture.cs
@@ -0,0 +1,8 @@
+﻿using Users.Api.Tests.ClassFixture;
+
+namespace Users.Api.Tests.CollectionFixture;
+
+[CollectionDefinition("Collection fixture to test behavior")]
+public class CollectionFixture : ICollectionFixture<GuidTestsFixture>
+{
+}
---
+++ test/Users.Api.Tests/CollectionFixture/CollectionFixtureBehaviorTests.cs
@@ -0,0 +1,58 @@
+﻿using Users.Api.Tests.ClassFixture;
+using Xunit.Abstractions;
+
+namespace Users.Api.Tests.CollectionFixture;
+
+// These 2 test classes are sharing the same fixture, which is good to separate test groups
+
+[Collection("Collection fixture to test behavior")]
+public class CollectionFixtureBehaviorTests
+{
+	private readonly ITestOutputHelper _testOutputHelper;
+	private readonly GuidTestsFixture _fixture;
+
+	public CollectionFixtureBehaviorTests(ITestOutputHelper testOutputHelper, GuidTestsFixture fixture)
+	{
+		_testOutputHelper = testOutputHelper;
+		_fixture = fixture;
+	}
+
+
+	[Fact]
+	public void ExampleTest1()
+	{
+		_testOutputHelper.WriteLine($"The Guid was: {_fixture.Id}");
+	}
+
+	[Fact]
+	public void ExampleTest2()
+	{
+		_testOutputHelper.WriteLine($"The Guid was: {_fixture.Id}");
+	}
+}
+
+[Collection("Collection fixture to test behavior")]
+public class CollectionFixtureBehaviorTestsAgain
+{
+	private readonly ITestOutputHelper _testOutputHelper;
+	private readonly GuidTestsFixture _fixture;
+
+	public CollectionFixtureBehaviorTestsAgain(ITestOutputHelper testOutputHelper, GuidTestsFixture fixture)
+	{
+		_testOutputHelper = testOutputHelper;
+		_fixture = fixture;
+	}
+
+
+	[Fact]
+	public void ExampleTest1()
+	{
+		_testOutputHelper.WriteLine($"The Guid was: {_fixture.Id}");
+	}
+
+	[Fact]
+	public void ExampleTest2()
+	{
+		_testOutputHelper.WriteLine($"The Guid was: {_fixture.Id}");
+	}
+}
