2ce918da1d5d1034f851851d67c2b616fa820978
Andrei Ciuvilin <aciuvilin@morneaushepell.com>
2024-05-09 10:38:59 -0400

    Add timeout skip and explanation

--- test/Users.Api.Tests/LongRunningTests.cs
+++ test/Users.Api.Tests/LongRunningTests.cs
@@ -2,7 +2,10 @@
 
 public class LongRunningTests
 {
-	[Fact(Timeout = 2000)]
+	// Timeouts normally are not used in Unit testing.
+	// Usually it is more common practice in Integration Testing
+	// But in some rare cases it might be useful in unit testing aswell
+	[Fact(Timeout = 2000, Skip = "This test normally fails by timeout, therefore skipped and kept as a reference.")]
 	public async Task SlowTest()
 	{
 		// Really Slow test (shouldn't take that long)
