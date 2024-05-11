40a1202daade22c0fcbe1e6b7328249d65cc887c
Andrei Ciuvilin <aciuvilin@morneaushepell.com>
2024-05-11 11:19:56 -0400

    Add Web app Integration tests

---
+++ test/Customers.WebApp.Tests/Pages/AddCustomerTests.cs
@@ -0,0 +1,56 @@
+﻿using Bogus;
+using Customers.WebApp.Models;
+using FluentAssertions;
+using Microsoft.Playwright;
+
+namespace Customers.WebApp.Tests.Pages;
+
+[Collection("Test collection")]
+public class AddCustomerTests
+{
+	private readonly SharedTestContext _testContext;
+
+	private readonly Faker<Customer> _customerGenerator = new Faker<Customer>()
+		.RuleFor(r => r.Email, faker => faker.Person.Email)
+		.RuleFor(r => r.FullName, faker => faker.Person.FullName)
+		.RuleFor(r => r.GitHubUsername, SharedTestContext.ValidGitHubUserName)
+		.RuleFor(r => r.DateOfBirth, faker => DateOnly.FromDateTime(faker.Person.DateOfBirth));
+
+
+	public AddCustomerTests(SharedTestContext testContext)
+	{
+		_testContext = testContext;
+	}
+
+	[Fact]
+	public async Task Create_ShouldCreateCustomer_WhenDataIsValid()
+	{
+		// Arrange
+		var page = await _testContext.Browser.NewPageAsync(new BrowserNewPageOptions
+		{
+			BaseURL = SharedTestContext.AppUrl
+		});
+		await page.GotoAsync("add-customer");
+		var customer = _customerGenerator.Generate();
+
+		// Act
+		await page.FillAsync("input[id=fullname]", customer.FullName);
+		await page.FillAsync("input[id=email]", customer.Email);
+		await page.FillAsync("input[id=github-username]", customer.GitHubUsername);
+		await page.FillAsync("input[id=dob]", customer.DateOfBirth.ToString("yyyy-MM-dd"));
+
+		await page.CheckAsync("button[type=submit]");
+
+		// Assert
+		var linkElement = page.Locator("article>p>a").First;
+		var link = await linkElement.GetAttributeAsync("href");
+		await page.GotoAsync(link!);
+
+		(await page.Locator("p[id=fullname-field]").InnerTextAsync()).Should().Be(customer.FullName);
+		(await page.Locator("p[id=email-field]").InnerTextAsync()).Should().Be(customer.Email);
+		(await page.Locator("p[id=github-username-field]").InnerTextAsync()).Should().Be(customer.GitHubUsername);
+		(await page.Locator("p[id=dob-field]").InnerTextAsync()).Should().Be(customer.DateOfBirth.ToString("yyyy-MM-dd"));
+
+		await page.CloseAsync();
+	}
+}
---
+++ test/Customers.WebApp.Tests/SharedTestContext.cs
@@ -0,0 +1,51 @@
+﻿using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Model.Common;
+using Ductus.FluentDocker.Services;
+using Microsoft.Playwright;
+
+namespace Customers.WebApp.Tests;
+
+public class SharedTestContext : IAsyncLifetime
+{
+	public const string ValidGitHubUserName = "validduser";
+	public const string AppName = "integration-app";
+	public const string AppUrl = "https://localhost:7780";
+
+	public GitHubApiServer GitHubApiServer { get; } = new();
+
+	private static readonly string DockerComposeFile =
+		Path.Combine(Directory.GetCurrentDirectory(), (TemplateString)"../../../docker-compose.integration.yml");
+
+	private IPlaywright _playwright;
+	public IBrowser Browser { get; private set; }
+
+	private readonly ICompositeService _dockerService = new Builder()
+		.UseContainer()
+		.UseCompose()
+		.FromFile(DockerComposeFile)
+		.RemoveOrphans()
+		.WaitForHttp(AppName, AppUrl)
+		.Build();
+
+	public async Task InitializeAsync()
+	{
+		GitHubApiServer.Start();
+		GitHubApiServer.SetupUser(ValidGitHubUserName);
+		_dockerService.Start();
+
+		_playwright = await Playwright.CreateAsync();
+		Browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+		{
+			Headless = false,
+			SlowMo = 1000
+		});
+	}
+
+	public async Task DisposeAsync()
+	{
+		await Browser.DisposeAsync();
+		_playwright.Dispose();
+		_dockerService.Dispose();
+		GitHubApiServer.Dispose();
+	}
+}
